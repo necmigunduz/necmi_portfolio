3b51a6ab6e2cb588667117b8ffaab9597b954bea
Necmi Gunduz <necmigunduz@gmail.com>
2025-09-12 15:57:54 +0300

    add id to about

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 export default function About() {
   return (
-    <section>
+    <section id='about'>
       <h1 className="text-3xl font-bold text-gray-900 mb-4">
         Hello and Welcome!
       </h1>
