2dbdd5556458e76040e433d27183ef4c9baca5f4
Necmi Gunduz <necmigunduz@gmail.com>
2026-02-12 18:50:22 +0300

    regulate: give some style to contact

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -2,19 +2,29 @@ import React from "react";
 
 export default function Contact() {
   return (
-    <section
-      id="contact"
-    >
+    <section id="contact">
+      <h2 className="text-2xl font-bold mb-4">Contact</h2>
       <h2 className="text-2xl font-bold mb-4">
         <a
           className="text-sky-500 text-3xl md:text-2xl sm:text-xl max-sm:text-lg font-semibold hover:underline"
           href="https://meetings-eu1.hubspot.com/necmi-gunduz"
-        >  Book an Appointment </a>
+        >
+          {" "}
+          Click here toBook an Appointment{" "}
+        </a>
       </h2>
-      <a href="mailto:gund.necmi@gmail.com">gund.necmi@gmail.com</a>
+      <p>
+        <span  className="text-sky-500 text-2xl font-bold mb-4">Or send me an Email:{" "}</span>
+        <span>
+          <a href="mailto:gund.necmi@gmail.com" className="text-sky-500 text-3xl md:text-2xl sm:text-xl max-sm:text-lg font-semibold hover:underline">gund.necmi@gmail.com</a>{" "}
+        </span>
+      </p>
+      <br />
+       <hr />
+       <br />
       <p className="text-sm mb-4 md:mb-0">
         © {new Date().getFullYear()} Necmi Gündüz. All rights reserved.
       </p>
     </section>
-  )
-};
+  );
+}
